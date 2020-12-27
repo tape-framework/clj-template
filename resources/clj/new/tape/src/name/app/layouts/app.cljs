9f8585d8032e1c1e0d0a173496be13f478316984
clyfe <>
2020-12-27 13:12:31 +0200

    Update for separate current view module

--- resources/clj/new/tape/src/name/app/layouts/app.cljs
+++ resources/clj/new/tape/src/name/app/layouts/app.cljs
@@ -2,12 +2,13 @@
   (:require [reagent.core :as r]
             [re-frame.core :as rf]
             [tape.mvc.view :as v]
+            [tape.current :as current]
             [tape.router :as router]
             [tape.toasts.view :as toasts.v]))
 
 
 (defn app []
-  (let [current-view-fn @(rf/subscribe [::v/current-fn])]
+  (let [current-view-fn @(rf/subscribe [::current/view-fn])]
     [:<>
      [:section.section
       [:div.container
